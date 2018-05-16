2c6cd79f37e4fdd4186fb9da6dae15a3289d426c
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-16 13:44:36 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -6,4 +6,16 @@ class App < Sinatra::Base
     erb :form
   end
 
+  post '/student' do
+  @student = Student.new(params[:student])
+
+  params[:student][:courses].each do |details|
+    Course.new(details)
+  end
+
+  @courses = Course.all
+
+  erb :student
+end
+
 end
