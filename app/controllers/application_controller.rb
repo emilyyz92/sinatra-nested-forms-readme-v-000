ee73a0ef648db03bcbf50c11f71ba05d734f8986
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-16 14:41:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ require_relative '../config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+   set :views, "app/views"
+   set :public_dir, "public"
+ end
+
   get './' do
     erb :form
   end
