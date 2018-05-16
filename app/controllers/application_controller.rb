91c58484562d4c2bb009ff79153c30171a193f5f
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-16 15:21:48 +0000

    Done.

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < Sinatra::Base
+
+  configure do
+   set :views, "app/views"
+   set :public_dir, "public"
+ end
+
+  get '/' do
+
+    erb :form
+  end
+
+  post '/student' do
+    @student = Student.new(params[:student])
+
+    params[:student][:courses].each do |details|
+      Course.new(details)
+    end
+
+    @courses = Course.all
+
+    erb :student
+  end
+
+end
